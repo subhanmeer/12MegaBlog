582d4a479d44acc8f07ccce44d2a99dd94cb0b13
subhanmeer <banimeer@gmail.com>
2024-05-20 13:57:54 -0700

    MegaProject

---
+++ src/appwrite/auth.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-useless-catch */
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            
+            if (userAccount) {
+            
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
+            console.error('Failed to create account', error);
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createSession(email, password);
+        } catch (error) {
+            console.error('Login failed', error);
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.error("Failed to get current user", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
