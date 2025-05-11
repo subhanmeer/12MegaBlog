e0adb519ec5647aacc3867f0ec0550c900dbf08b
subhanmeer <banimeer@gmail.com>
2025-05-11 13:14:08 -0700

    changes

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React,{ useEffect, useState } from "react";
 import { Container, PostCard } from "../components";
 import appwriteService from "../appwrite/config";
 
